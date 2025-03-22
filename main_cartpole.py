96ca4b5abc4849b61b75688b1fd3762acf6661e7
J.E. Remy <jeanerolle97@gmail.com>
2025-03-22 22:07:02 +0100

    Update main_cartpole.py

--- main_cartpole.py
+++ main_cartpole.py
@@ -37,7 +37,7 @@ def main():
         'epsilon_decay': 0.995,
         'batch_size': 64,
         'learning_rate': 0.001,
-        'memory_size': 10000
+        'memory_size': 10000,
         'hidden_size': 128
     }
 
