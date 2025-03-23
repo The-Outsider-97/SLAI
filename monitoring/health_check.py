39ba5a941a7e5763fbd72e70c093e8c8c4b3f345
J.E. Remy <jeanerolle97@gmail.com>
2025-03-23 01:39:58 +0100

    Update health_check.py

--- monitoring/health_check.py
+++ monitoring/health_check.py
@@ -1,1 +1,14 @@
+import logging
+import socket
 
+def service_health_check(host="localhost", port=8000):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((host, port))
+    sock.close()
+    return result == 0
+
+if __name__ == "__main__":
+    if service_health_check():
+        print("Service is healthy!")
+    else:
+        print("Service is down!")
