53ea73c3341520a69c9563ef0a401779f7807d71
J.E. Remy <jeanerolle97@gmail.com>
2025-03-25 01:14:00 +0100

    Update main_safe_ai.py

--- main_safe_ai.py
+++ main_safe_ai.py
@@ -8,6 +8,9 @@ from modules.model_trainer import ModelTrainer
 from modules.security_manager import SecurityManager
 from modules.monitoring import Monitoring
 from modules.compliance_auditor import ComplianceAuditor
+from rnd_loop.evaluator import Evaluator
+from rnd_loop.experiment_manager import ExperimentManager
+from rnd_loop.hyperparam_tuner import HyperparamTuner
 from deployment.git.rollback_handler import RollbackHandler
 from agent.safe_ai_agent import SafeAI_Agent
 from collaborative.shared_memory import SharedMemory
