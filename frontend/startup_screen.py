58c39817f5e9bf2a4c4b300488c1bf8464ad4e9a
J.E. Remy <jeanerolle97@gmail.com>
2025-03-26 21:04:21 +0100

    Update startup_screen.py

--- frontend/startup_screen.py
+++ frontend/startup_screen.py
@@ -18,7 +18,7 @@ class StartupScreen(QWidget):
         self.label.setAlignment(Qt.AlignCenter)
 
         # Load GIF animation
-        self.movie = QMovie("Frontend/assets/launch_animation.gif")  # Place your gif here
+        self.movie = QMovie("assets/startup/png")
         self.label.setMovie(self.movie)
         self.movie.start()
 
