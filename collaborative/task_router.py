b9c62a95b53b23754602e6fbdbe23beb5c8245e3
J.E. Remy <jeanerolle97@gmail.com>
2025-03-26 09:38:26 +0100

    Update task_router.py

--- collaborative/task_router.py
+++ collaborative/task_router.py
@@ -12,7 +12,16 @@ class TaskRouter:
     def __init__(self, registry: AgentRegistry, shared_memory=None):
         self.registry = registry
         self.shared_memory = shared_memory or {}
-  
+
+    def get_agents_by_task(self, task_type):
+        """
+        Return all agents that support a given task_type.
+        """
+        return {
+            name: agent for name, agent in self._agents.items()
+            if task_type in self._capabilities.get(name, [])
+        }
+    
     def route(self, task_type, task_data):
         eligible_agents = self.registry.get_agents_by_task(task_type)
 
