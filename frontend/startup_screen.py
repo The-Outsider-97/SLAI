2fe16a17b5ee02dd9bd514799a27f9e38067af72
J.E. Remy <jeanerolle97@gmail.com>
2025-03-26 23:52:52 +0100

    Update startup_screen.py

--- frontend/startup_screen.py
+++ frontend/startup_screen.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QMovie
-import sys
+import os, sys
 
 class StartupScreen(QWidget):
-    def __init__(self, on_finish_callback):
+    def __init__(self, on_ready_to_proceed):
         super().__init__()
-        self.on_finish_callback = on_finish_callback
+        self.on_ready_to_proceed = on_ready_to_proceed
+        self.ready_to_show = False
+        self.minimum_time_elapsed = False
         self.initUI()
 
     def initUI(self):
@@ -17,18 +19,21 @@ class StartupScreen(QWidget):
         self.label = QLabel(self)
         self.label.setAlignment(Qt.AlignCenter)
 
-        # Load GIF animation
-        self.movie = QMovie("assets/startup.png")
-        self.label.setMovie(self.movie)
-        self.movie.start()
+        gif_path = os.path.join(os.path.dirname(__file__), "assets", "startup.png")
+        if os.path.exists(gif_path):
+            self.movie = QMovie(gif_path)
+            self.label.setMovie(self.movie)
+            self.movie.start()
+        else:
+            self.label.setText("Loading SLAI...")
+            self.label.setStyleSheet("color: white; font-size: 18px;")
 
         layout.addWidget(self.label)
         self.setLayout(layout)
-        self.resize(600, 400)
+        self.resize(2560, 1440)
         self.center_on_screen()
 
-        # Timer: change to your desired splash duration (ms)
-        QTimer.singleShot(3500, self.finish)
+        QTimer.singleShot(10000, self.allow_proceed)  # Wait 10s before allowing transition
 
     def center_on_screen(self):
         screen = QApplication.primaryScreen().geometry()
@@ -36,6 +41,16 @@ class StartupScreen(QWidget):
         y = (screen.height() - self.height()) // 2
         self.move(x, y)
 
-    def finish(self):
+    def allow_proceed(self):
+        self.minimum_time_elapsed = True
+        if self.ready_to_show:
+            self.proceed()
+
+    def notify_launcher_ready(self):
+        self.ready_to_show = True
+        if self.minimum_time_elapsed:
+            self.proceed()
+
+    def proceed(self):
         self.close()
-        self.on_finish_callback()
+        self.on_ready_to_proceed()
