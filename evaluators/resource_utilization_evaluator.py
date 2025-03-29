18112bab773d38e2784d9fb72c8a0eed39f21b9a
J.E. Remy <jeanerolle97@gmail.com>
2025-03-29 10:45:11 +0100

    Create resource_utilization_evaluator.py

---
+++ evaluators/resource_utilization_evaluator.py
@@ -0,0 +1,5 @@
+class ResourceUtilizationEvaluator:
+    def evaluate(self, outputs, ground_truths):
+        # Placeholder: simulate resource usage (0 = optimal)
+        import random
+        return 1.0 - random.random() * 0.1  # Fake a 'good' score near 1.0
