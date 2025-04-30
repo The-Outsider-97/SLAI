e19b1ad04823db817f73f36e41bdf04f96965b15
J.E. Remy <jeanerolle97@gmail.com>
2025-04-30 20:00:24 +0200

    Create knowledge_cache.py

---
+++ src/agents/knowledge/knowledge_cache.py
@@ -0,0 +1,28 @@
+
+import hashlib
+
+from typing import Optional, Any
+from collections import OrderedDict
+
+class KnowledgeCache:
+    """Lightweight cache with LRU eviction policy"""
+    def __init__(self, max_size: int = 100):
+        self.cache = OrderedDict()
+        self.max_size = max_size
+
+    def get(self, key: str) -> Optional[Any]:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return None
+
+    def set(self, key: str, value: Any) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.max_size:
+            self.cache.popitem(last=False)
+
+    def hash_query(self, query: str) -> str:
+        """Semantic hashing for similar queries (simplified)"""
+        return hashlib.md5(query.encode()).hexdigest()
