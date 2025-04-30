3b43ec47dcd4cc5c0e70100df02219d24d737258
J.E. Remy <jeanerolle97@gmail.com>
2025-05-01 00:29:11 +0200

    Update knowledge_cache.py

--- src/agents/knowledge/knowledge_cache.py
+++ src/agents/knowledge/knowledge_cache.py
@@ -1,6 +1,9 @@
 
+import os
+import json
 import hashlib
 
+from cryptography.fernet import Fernet
 from typing import Optional, Any
 from collections import OrderedDict
 
@@ -9,14 +12,20 @@ class KnowledgeCache:
     def __init__(self, max_size: int = 100):
         self.cache = OrderedDict()
         self.max_size = max_size
+        self.cipher = Fernet(os.getenv('CACHE_ENCRYPTION_KEY', Fernet.generate_key()))
 
     def get(self, key: str) -> Optional[Any]:
+        encrypted = super().get(key)
+        if encrypted:
+            return json.loads(self.cipher.decrypt(encrypted).decode())
         if key in self.cache:
             self.cache.move_to_end(key)
             return self.cache[key]
         return None
 
     def set(self, key: str, value: Any) -> None:
+        encrypted = self.cipher.encrypt(json.dumps(value).encode())
+        super().set(key, encrypted)
         if key in self.cache:
             self.cache.move_to_end(key)
         self.cache[key] = value
