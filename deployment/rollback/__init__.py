0d6afd54a7120c628eb77ef28c919e20a5440731
J.E. Remy <jeanerolle97@gmail.com>
2025-05-25 23:42:03 +0200

    Update __init__.py

--- deployment/rollback/__init__.py
+++ deployment/rollback/__init__.py
@@ -1,2 +1,11 @@
-from .model_rollback import rollback_model
-from .code_rollback import rollback_to_previous_tag, reset_to_commit, delete_tag
+from .code_rollback import (
+    AtomicRollback,
+    reset_to_commit,
+    delete_tag,
+    rollforward_to_next_tag
+)
+from .model_rollback import (
+    rollback_model,
+    validate_backup_integrity,
+    rollforward_model
+)
