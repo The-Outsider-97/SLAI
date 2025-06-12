6a974528bbb2ab7edb9c6f1e66fad1f846b05a1c
J.E. Remy <jeanerolle97@gmail.com>
2025-06-12 22:08:46 +0200

    Create execution_memory.py

---
+++ src/agents/execution/execution_memory.py
@@ -0,0 +1,23 @@
+
+
+from src.agents.execution.utils.config_loader import load_global_config, get_config_section
+from logs.logger import get_logger, PrettyPrinter
+
+logger = get_logger("Execution Memory")
+printer = PrettyPrinter
+
+class ExecutionMemory:
+    def __init__(self):
+        self.config = load_global_config()
+        self.manager_config = get_config_section('execution_memory')
+
+        logger.info(f"Execution Manager succesfully initialized")
+
+if __name__ == "__main__":
+    print("\n=== Running Execution Memory Test ===\n")
+    printer.status("Init", "Execution Memory initialized", "success")
+
+    memory = ExecutionMemory()
+    print(memory)
+
+    print("\n=== Simulation Complete ===")
