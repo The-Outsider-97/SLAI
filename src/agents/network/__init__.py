53a148d624f149ff07af0529350f6b292ab6c29c
J.E. Remy <jeanerolle97@gmail.com>
2026-04-17 14:16:07 +0200

    Refactor __init__.py to include new network components

--- src/agents/network/__init__.py
+++ src/agents/network/__init__.py
@@ -1,10 +1,34 @@
 from .network_memory import NetworkMemory, MemoryEntry
-from .network_metrics import NetworkMetrics
-from .network_policy import NetworkPolicy
+from .network_adapters import AdapterRegistrySpec, ManagedAdapterRecord, NetworkAdapters
+from .network_lifecycle import NetworkLifecycle
+from .network_metrics import NetworkMetrics, TelemetryEvent, RollingLatencyWindow, MetricAggregate
+from .network_policy import NetworkPolicy, PolicyCheckResult, RateLimitBucket
+from .network_reliability import ReliabilityDecision, NetworkReliability
+from .network_stream import StreamSessionRecord, NetworkStream
 
 __all__ = [
+    # Memory
     "NetworkMemory",
     "MemoryEntry",
+    # Adapters
+    "AdapterRegistrySpec",
+    "ManagedAdapterRecord",
+    "NetworkAdapters",
+    # Lifecycle
+    "NetworkLifecycle",
+    # Metrics
     "NetworkMetrics",
+    "TelemetryEvent",
+    "RollingLatencyWindow",
+    "MetricAggregate",
+    # Policy
     "NetworkPolicy",
+    "PolicyCheckResult",
+    "RateLimitBucket",
+    # reliability
+    "ReliabilityDecision",
+    "NetworkReliability",
+    # Stream
+    "StreamSessionRecord",
+    "NetworkStream",
 ]
