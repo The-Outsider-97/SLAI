5d7ccd4dfd724e258632f25d73ac54065e88bd11
J.E. Remy <jeanerolle97@gmail.com>
2026-08-24 20:09:05 +0200

    Restore imports and exports in __init__.py

--- src/agents/qnn/utils/__init__.py
+++ src/agents/qnn/utils/__init__.py
@@ -2,15 +2,15 @@
 
 from .quantum_errors import *
 from .quantum_helpers import *
-# from .config_loader import *
+from .config_loader import *
 
 from .quantum_errors import __all__ as _quantum_error_exports
 from .quantum_helpers import __all__ as _quantum_helper_exports
-# from .config_loader import __all__ as _config_loader_exports
+from .config_loader import __all__ as _config_loader_exports
 
 
 __all__ = [
     *_quantum_error_exports,
     *_quantum_helper_exports,
-#     *_config_loader_exports,
+    *_config_loader_exports,
 ] # type: ignore
